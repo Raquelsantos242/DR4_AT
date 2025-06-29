a59145b7a39dc0b4a5c06d45d9fc80669444331b
Raquel Braga <87723504+Raquelsantos242@users.noreply.github.com>
2025-06-29 15:09:52 -0300

    Update App.java

--- DR4_AT_4/src/main/java/infnet/selenium/App.java
+++ DR4_AT_4/src/main/java/infnet/selenium/App.java
@@ -1,23 +1,15 @@
 package infnet.selenium;
 
-public class App 
+public class App
 {
-        public static void main(String[] args) {
-            Address address = new Address("Av. Paulista, 1000", "São Paulo", "01310-100");
+    public static void main(String[] args) {
+        Address address = new Address("Av. Presidente Vargas, 1000", "Rio de Janeiro", "20071-000");
 
-            System.out.println("Endereço cadastrado:");
-            System.out.println("Rua: " + address.getStreet());
-            System.out.println("Cidade: " + address.getCity());
-            System.out.println("CEP: " + address.getZipCode());
 
-            address.setStreet("Rua Augusta, 1500");
-            address.setCity("São Paulo");
-            address.setZipCode("01305-100");
+        System.out.println("\nEndereço cadastrado:");
+        System.out.println("Rua: " + address.getStreet());
+        System.out.println("Cidade: " + address.getCity());
+        System.out.println("CEP: " + address.getZipCode());
 
-            System.out.println("\nEndereço atualizado:");
-            System.out.println("Rua: " + address.getStreet());
-            System.out.println("Cidade: " + address.getCity());
-            System.out.println("CEP: " + address.getZipCode());
-        }
     }
-
+}
